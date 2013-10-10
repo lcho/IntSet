5dfc0441941edec25eaa23aa9d0e5e7a3739a9b8
lcho <Lance@d-69-91-217-237.dhcp4.washington.edu>
2013-10-10 16:16:11 -0700

    Edited the intset.cpp

---
+++ intset.cpp
@@ -0,0 +1,202 @@
+//
+//  inset.cpp
+//  lab2
+//
+//  Created by Terence Schumacher on 10/8/13.
+//  Copyright (c) 2013 Terence Schumacher. All rights reserved.
+//
+#include "intset.h"
+
+
+IntSet::IntSet(int value1, int value2, int value3, int value4, int value5){
+    
+    int values[] = {value1, value2, value3, value4, value5};
+    int max = 0;
+    for (int counter = 0; counter < 5; counter++){
+        if (values[counter] > max){
+            max = values[counter];
+        }
+        
+        // for testting - please remove
+        // cout << max << endl;
+    }
+    
+    set = new bool[max+1];
+    size = max+1;
+    for(int counter=0; counter < size; counter++){
+        set[counter] = false;
+        // cout << set[counter] << endl;
+    }
+    for(int counter=0; counter < 5; counter++){
+        if (values[counter] >= 0){
+            set[ values[counter] ] = true;
+        }
+        
+    }
+    
+    /*  for testing
+     for(int counter=0; counter < size; counter++){
+     cout << set[counter] << " ";
+     }
+     */
+    
+    
+    
+    
+    /* not needed - please remove
+     number = value < 0 ? 0: value;
+     
+     bool set[SIZE];
+     for (int i = 0; i < sizeof(set); i++) {
+     
+     set[i] = false;
+     }
+     */
+}
+
+
+//----------------------------------------------------------------------------
+// getSize()
+// returns the private data 'size' of the IntSet
+int IntSet::getSize() const {
+    return size;
+}
+
+
+//----------------------------------------------------------------------------
+// operator+
+// overloaded +: addition of 2 IntSets
+IntSet IntSet::operator+(const IntSet & add) const{
+    IntSet addition (-1);
+    // A+B = adds all numbers in both sets
+    if (size  > add.getSize()){
+        for(int index = 0; index <= add.getSize(); index++){
+            if(add.set[index]== true){
+                set[index] = true;
+            }
+        }
+        addition.set = set;
+        
+    }else{
+        for(int index = 0; index <= size; index++){
+            if(set[index]== true){
+                add.set[index] = true;
+            }
+        }
+        addition.set = add.set;
+    }
+    
+    return addition;
+}
+
+
+//----------------------------------------------------------------------------
+// operator*
+// overloaded *: multiplication of 2 Rationals, current object and parameter
+IntSet IntSet::operator*(const IntSet & m) const{
+    int tempSize = 0;
+    bool * temp;
+    IntSet intersect (-1);
+    
+    // A*B = goes inside this if statement if A's size is bigger
+    if (size  > m.getSize()){
+        for(int index = m.getSize()-1; index >= 0; index--){
+            if(set[index]==true && m.set[index]==true){
+                tempSize = index + 1;
+                break;
+            }
+        }
+        temp = new bool [tempSize];
+        for (int index = 0; index < tempSize; index++){
+            temp[index] = false;
+            if( set[index] == true && m.set[index] == true){
+                temp[index] = true;
+            }
+            //cout << temp[index];
+        }
+    }else{  // A*B = goes inside this else statement if A's size is smaller
+        
+        for(int index = size-1; index >= 0; index--){
+            if(set[index]==true && m.set[index]==true){
+                tempSize = index + 1;
+                break;
+            }
+        }
+        temp = new bool [tempSize];
+        for (int index = 0; index < tempSize; index++){
+            temp[index] = false;
+            if( set[index] == true && m.set[index] == true){
+                temp[index] = true;
+            }
+            
+            // for testing - please remove
+            // cout << temp[index];
+        }
+        
+    }
+    intersect.set = temp;
+    
+    return intersect;
+}
+
+
+
+//----------------------------------------------------------------------------
+// operator<<
+// overloaded <<: prints the set
+
+ostream& operator<<(ostream &output, const IntSet& s) {
+    output << "{ ";
+    for (int counter = 0; counter < s.size; counter++){
+        output << s.set[counter] << " ";
+    }
+    output << "}";
+    return output;
+}
+
+
+
+
+//----------------------------------------------------------------------------
+// operator>>
+// overloaded >>:  Not Yet Done
+istream& operator>>(istream &input, IntSet &s){
+    while(!input.eof()) {
+        int temp = 0;
+        input >> temp;
+        if (temp < 0){
+            break;
+        }
+        s.set[temp] = true;
+    }
+    return input;
+}
+
+
+
+/*
+ 
+ IntSet::IntSet(int value1, int value2, int value3, int value4, int value5){
+ 
+ int values[] = {value1, value2, value3, value4, value5};
+ int max = 0;
+ for (int counter = 0; counter < 5; counter++){
+ if (values[counter] > max){
+ max = values[counter];
+ }
+ }
+ 
+ set = new bool[max+1];
+ size = max+1;
+ for(int counter=0; counter < size; counter++){
+ set[counter] = false;
+ }
+ for(int counter=0; counter < 5; counter++){
+ if (values[counter] >= 0){
+ set[ values[counter] ] = true;
+ }
+ }
+ }
+ 
+ 
+ */

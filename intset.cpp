17e7c40aa3b93ad2848365e6ff4a1471c71a84a8
lcho <cholance1022@gmail.com>
2013-10-13 01:42:08 -0700

    Update intset.cpp

--- intset.cpp
+++ intset.cpp
@@ -356,7 +356,7 @@ bool IntSet::isInSet(int number){
 ostream& operator<<(ostream &output, const IntSet& s) {
     output << "{ ";
     for (int counter = 0; counter < s.size; counter++){
-        output << s.set[counter] << " ";
+        output << counter << " ";
     }
     output << "}";
     return output;
